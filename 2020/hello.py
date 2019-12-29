aa28b324df956099066d61915697899b0930b18a
Jose Apablaza <japablaza@gmail.com>
2019-12-29 00:37:48 -0600

    Hello with main and test

--- 2020/hello.py
+++ 2020/hello.py
@@ -1,1 +1,22 @@
-print('Hello, World!')
+#!/usr/bin/env python3
+# Purpose: Say hello
+
+import sys
+
+
+def greet(name):
+    return f'Hola, {name}!'
+
+
+def test_greet():
+    assert greet('World') == 'Hola, World!'
+    assert greet('Terra Firma') == 'Hola, Terra Firma!'
+
+
+def main():
+    args = sys.argv[1:]
+    name = args[0] if args else 'World'
+    print(greet(name))
+
+if __name__ == '__main__':
+    main()
