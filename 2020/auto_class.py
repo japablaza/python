b85dece5739d1d0ec4cea802cec2a7f6c944552c
Jose Apablaza <japablaza@gmail.com>
2020-03-01 13:44:34 -0600

    Seguda Clase y Objetos. Su Lada

--- 2020/auto_class.py
+++ 2020/auto_class.py
@@ -13,8 +13,18 @@ class auto:
     valor = 800000000000000
 
     def descripcion(self):
-        desc_str = 'El %s es una %s de color %s. Tiene un valor de %d pesos.' % (self.nombre, self.tipo, self.color, self.valor)
+        desc_str = 'El %s es una %s de color %s. Tiene un valor de $%.2f pesos.' % (self.nombre, self.tipo, self.color, self.valor)
         return desc_str
 
 
 print(auto().descripcion())
+
+auto1 = auto()
+auto2 = auto()
+
+auto1.nombre = 'Lada'
+auto1.color = 'rojo'
+auto1.tipo = 'convertible'
+auto1.valor = 60000.00
+
+print(auto1.descripcion())
