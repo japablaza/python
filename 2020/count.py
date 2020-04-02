3853ab84b7b2dea25ce6782d0c8abe980ff19daf
Jose Apablaza <japablaza@gmail.com>
2020-04-02 10:43:18 -0500

    Testing python idiomas: count

---
+++ 2020/count.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import math
+
+numero_letras = 0
+texto = 'Una vez te vi pasar'
+for n in texto:
+    numero_letras += 1
+
+print('Numero de letras: ',numero_letras)
+
+print(f'Pi: {math.pi:0.02f}')
---
+++ 2020/review.md
@@ -0,0 +1,4 @@
+CHAP01
+
+- 1.5 The first paragraph did not make much sense
+- 1.7 Typo in the second paragraph, double 'as'
