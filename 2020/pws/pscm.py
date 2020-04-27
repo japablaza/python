783558f2bb132ccd821dcb8211562f23bcf54339
Jose Apablaza <japablaza@gmail.com>
2020-04-27 14:19:00 -0500

    string format with f""

--- 2020/pws/pscm.py
+++ 2020/pws/pscm.py
@@ -6,3 +6,5 @@
 saludo = 'hola'
 despido = 'chao'
 print('Yo saludo con \'{}\' y me despido con \'{}\''.format(saludo, despido))
+print('+'*5)
+print(f'Yo saludo con {saludo} y me despido con {despido}')
