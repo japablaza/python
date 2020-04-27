9d97b6f0d76d1ddbff2d79d76d8d098152b97810
Jose Apablaza <japablaza@gmail.com>
2020-04-27 15:18:13 -0500

    strings and format

--- 2020/pws/pscm.py
+++ 2020/pws/pscm.py
@@ -8,3 +8,12 @@ despido = 'chao'
 print('Yo saludo con \'{}\' y me despido con \'{}\''.format(saludo, despido))
 print('+'*5)
 print(f'Yo saludo con {saludo} y me despido con {despido}')
+print('*'*5)
+
+print(saludo.upper())
+print(saludo.capitalize())
+print(saludo.count('a'))
+print('*'*5)
+
+nombre = input('Como te llamas? ')
+print(f'Tu nombre es {nombre}')
