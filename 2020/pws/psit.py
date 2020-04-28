a4af64fc5151feedb29cc7d634586cbbed9a87f2
Jose Apablaza <japablaza@gmail.com>
2020-04-28 14:41:33 -0500

    Done with 101 boolean and if else

--- 2020/pws/psit.py
+++ 2020/pws/psit.py
@@ -24,8 +24,22 @@ print(a < b)
 print(a > b)
 print(a <= b)
 
+# edad = input('Que edad tienes? ')
+# if int(edad) >= 66:
+#     print(texto)
+# else:
+#     print(frase)
+
 edad = input('Que edad tienes? ')
-if int(edad) >= 66:
-    print(texto)
+edad = int(edad)
+if edad < 10:
+    print('Have a fun day!!')
+elif edad >=10 and edad < 18:
+    print('Almost there')
+elif edad >= 18 and edad < 21:
+    print('Ready for the army')
+    print('Still a baby for beer')
+elif edad == 21:
+    print('Ready for a bright future!')
 else:
-    print(frase)
+    print('Too old')
