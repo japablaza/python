0ff9f2b1de5e2d5798405e947f94a11133405055
Jose Apablaza <japablaza@gmail.com>
2024-05-13 18:58:16 -0500

    Strings

---
+++ 2024/texto.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# Comentarios
+# String Type
+
+oracion = "Los cuentos de la madre tierra"
+print("Lista de metodos")
+string_dir = oracion.__dir__()
+# print(string_dir)
+# num = 1
+# for metodo in string_dir:
+#     print(f"{num}. {metodo}")
+#     num += 1
+
+print
+print(oracion.__repr__())
