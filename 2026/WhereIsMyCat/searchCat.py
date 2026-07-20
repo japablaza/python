960bd9fe276d4519aacaad63fd352d0b672e7ed4
Jose Apablaza <japablaza@gmail.com>
2026-07-20 17:23:28 -0500

    renaming file

--- 2026/WhereIsMyCat/searchCat.py
+++
@@ -1,6 +0,0 @@
-import socket, warnings
-
-try:
-    socket.setdefaulttimeout(1)
-    socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(('1.1.1.1',53))
-except socket.error as ex:  raise Exception("No hay connecion a Internet, verificat el cable o cambiate a un mejor servicio")
